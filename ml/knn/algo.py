b76f5e7fcc8cf1c58ba2b11ccce135b491770108
Khalid Hamzaoui <kh.hamzaoui@gmail.com>
2016-02-12 02:09:05 +0100

    knn : improve modularity

--- ml/knn/algo.py
+++ ml/knn/algo.py
@@ -10,9 +10,9 @@ int(miles flown) \t float(hours of game) \t float(litters of icecream) \t ['larg
 e.g. :
 40920	8.326976	0.953952	largeDoses
 """
-def nearestNeighbors(elt, k, file_in):
+labelNames = { -1 : "didntLike", 0 : "smallDoses", 1 : "largeDoses" }
+def parseData(file_in):
     # parse file
-    labelNames = { -1 : "didntLike", 0 : "smallDoses", 1 : "largeDoses" }
     data, labels = [], []
     with open(file_in, 'r') as f:
         try:
@@ -31,27 +31,33 @@ def nearestNeighbors(elt, k, file_in):
     ad = array(data)
     mins, maxs = ad.min(axis=0), ad.max(axis=0)
     had = (ad - mins) / (maxs - mins)
-    helt = (elt - mins) / (maxs - mins)
+    return labels, had, mins , maxs
 
     # compute distances from elt
+def classifyEltKNN(labels, had, mins, maxs, elt, k):
+    helt = (elt - mins) / (maxs - mins)
     diff = had - helt
     arSqDiff = diff ** 2
     arSsqDiff = arSqDiff.sum(axis=1)
     arRsqDiff = arSsqDiff**0.5
 
     sortedDistIdx = arRsqDiff.argsort()
+
     labelsCnt = { l : 0 for l in labels }
 
     for i in range(min(k, arRsqDiff.size)) :
         labelsCnt[labels[sortedDistIdx[i]]] += 1
     sortedLabelsCnt = sorted( labelsCnt.iteritems(), key=operator.itemgetter(1), reverse=True)
 
-    print(sortedLabelsCnt[0][0])
-    print(labelNames[sortedLabelsCnt[0][0]])
+    print(str(sortedLabelsCnt[0][0]) + "  " + labelNames[sortedLabelsCnt[0][0]])
 
 
 ## return label for an element based on its knn in the data
-nearestNeighbors([400, 0.8, 0.5], 10, sys.argv[1])
+labels, had, mins , maxs = parseData(sys.argv[1])
+k = 20
+elts = [[400, 0.8, 0.5], [134000, 12, 0.9], [20000, 0, 1.1], [32000, 67, 0.1]]
+for elt in elts:
+    classifyEltKNN(labels, had, mins , maxs, elt, k)
 
 
 
